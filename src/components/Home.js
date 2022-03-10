d9fe1ed51777048f6cb344a2a2781e4e95b21226
serref jaguraga <reef163@gmail.com>
2022-03-10 16:42:00 -0500

    done

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,11 @@
 import React from "react";
 import "./Home.css";
 function Home() {
-  const mystyle = {
-    color: "white",
-    backgroundColor: "DodgerBlue",
-    padding: "10px",
-    fontFamily: "Georgia, serif",
-  };
   return (
-    <div className="home">
-      <h1 style={mystyle}>MOVIE APP</h1>
-      <img></img>
+    <div className="stars twinkling clouds">
+      <div className="home-title">
+        <span>MOVIE NIGHT</span>
+      </div>
     </div>
   );
 }
